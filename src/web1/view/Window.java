7104841258e7e11cf598e67776e963508777fc71
s20635 <mikolaj.mialkowski@outlook.com>
2021-03-31 17:44:10 +0200

    final refactor

--- src/web1/view/Window.java
+++ src/web1/view/Window.java
@@ -83,7 +83,6 @@ public class Window extends Application {
                 service.getWeather(cityTextFiled.getText());
                 initializeDisplayInfoScene(primaryStage);
                 primaryStage.setScene(displayInfoScene);
-
             }
         });
 
